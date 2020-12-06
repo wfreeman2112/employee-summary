9a800e47506afccf568b3d67e809128219796670
Wendi Freeman <wfreeman2112@gmail.com>
2020-12-06 09:34:34 -0600

    adding questions for the user

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+// array of questions for user
+const questions = [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the employee name?",
+    },
+  
+    {
+      type: "input",
+      name: "role",
+      message: "Describe your What is the employee's role?",
+    },
+  
+    {
+        type: "input",
+        name: "ID",
+        message: "ID number?",
+      },
+
+    {
+        type: "input",
+        name: "specific",
+        message: "?",
+      },
